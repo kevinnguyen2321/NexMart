8b401c8b1ff419c204c4deb9b09b5e62e79fb735
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2025-01-17 09:45:31 -0600

    Completed search for product feature

--- client/src/components/search/Results.jsx
+++ client/src/components/search/Results.jsx
@@ -2,11 +2,16 @@ import { useContext, useEffect, useState } from 'react';
 import SearchContext from '../context/SearchContextComp';
 import { getAllProducts } from '../../managers/productManager';
 import './Results.css';
+import { ProductDetails } from '../products/ProductDetails';
+import { useCart } from '../context/useCart';
 
 export const Results = () => {
   const [products, setProducts] = useState([]);
+  const [isProductDetailsOpen, setIsProductDetailsOpen] = useState(false);
+  const [selectedProductId, setSelectedProductId] = useState(null);
 
   const { keyword } = useContext(SearchContext);
+  const { addItemToCart } = useCart();
 
   useEffect(() => {
     getAllProducts().then((data) => setProducts(data));
@@ -20,6 +25,16 @@ export const Results = () => {
       )
     : products; // Show all products if no keyword
 
+  const handleProductDetailsToggle = (productId) => {
+    setIsProductDetailsOpen((prev) => !prev);
+    setSelectedProductId(productId);
+  };
+
+  const handleCloseProductDetailModal = () => {
+    setIsProductDetailsOpen(false);
+    setSelectedProductId(null);
+  };
+
   return (
     <>
       {keyword && <p>Results for "{keyword}"</p>}
@@ -30,11 +45,25 @@ export const Results = () => {
             <img src={product.imageUrl} alt={product.name} />
             <p>${product.price.toFixed(2)}</p>
             <div className="feature-product-button-wrapper">
-              <button>View</button>
-              <button>Add to cart</button>
+              <button onClick={() => handleProductDetailsToggle(product.id)}>
+                View
+              </button>
+              <button onClick={() => addItemToCart(product)}>
+                Add to cart
+              </button>
             </div>
           </div>
         ))}
+        {filteredProducts.length < 1 && <p>No results found for "{keyword}"</p>}
+      </div>
+      <div>
+        {isProductDetailsOpen && (
+          <ProductDetails
+            isProductDetailsOpen={isProductDetailsOpen}
+            selectedProductId={selectedProductId}
+            onClose={handleCloseProductDetailModal}
+          />
+        )}
       </div>
     </>
   );
